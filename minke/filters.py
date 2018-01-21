4b3641c8c125d819c9ebbb2bd6b7fc6c8a073af9
Thomas Leichtuss <leichtfuss@systopia.de>
2018-01-21 18:47:17 +0100

    wrote a real minke-status-filter...

--- minke/filters.py
+++ minke/filters.py
@@ -1,25 +1,63 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
+from .messages import get_msgs
 
-class SelectStatus(admin.SimpleListFilter):
-    title = _('Select Status')
+
+class StatusFilter(admin.SimpleListFilter):
+    title = _('Minke-Status')
     parameter_name = 'minkestatus'
-    template = 'minke/selectstatus.html'
+
+    def __init__(self, request, params, model, model_admin):
+        super(StatusFilter, self).__init__(request, params, model, model_admin)
+        self.states = ('success', 'warning', 'error')
+
+        # are there messages for this model?
+        try:
+            assert bool(request.session['minke'][model.__name__])
+        except (AssertionError, KeyError):
+            self.msgs = None
+        else:
+            self.msgs = request.session['minke'][model.__name__]
 
     def has_output(self):
-        return True
+        return bool(self.msgs)
+
+    def values(self):
+        return self.value().split(',') if self.value() else list()
+
+    def queryset(self, request, queryset):
+        if self.value():
+            ids = list()
+            for status in self.values():
+                ids += [int(id) for id, m in self.msgs.items() if m['status'] == status]
+            return queryset.filter(id__in=ids)
+        else:
+            return queryset
+
+    def choices(self, changelist):
+        yield {
+            'selected': not self.values(),
+            'query_string': changelist.get_query_string({}, [self.parameter_name]),
+            'display': _('All'),
+        }
+        for status in self.states:
+            selected = status in self.values()
+            if selected:
+                values = set(self.values()) - set([status])
+            else:
+                values = set(self.values()) | set([status])
+                print values, selected, status
+            if values:
+                new_param = {self.parameter_name: ','.join(values)}
+            else:
+                new_param = dict()
+            query_string = changelist.get_query_string(new_param, [self.parameter_name])
+            yield {
+                'selected': selected,
+                'query_string': query_string,
+                'display': status.title(),
+            }
 
     def lookups(self, request, model_admin):
         return tuple()
-
-    def queryset(self, request, queryset):
-        return queryset
-
-    def choices(self, changelist):
-        for status in ['success', 'warning', 'error']:
-            yield {
-                'query_string': changelist.get_query_string({}, [self.parameter_name]),
-                'display': status.title(),
-                'status': status,
-            }
