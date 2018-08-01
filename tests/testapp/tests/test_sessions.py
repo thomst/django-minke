a569ea9a438eb026a2c33cde2584a8fe0ddf4e9d
Thomas Leichtuss <leichtfuss@systopia.de>
2018-08-01 10:04:47 +0200

    added tests for UnicodeResult

--- tests/testapp/tests/test_sessions.py
+++ tests/testapp/tests/test_sessions.py
@@ -3,11 +3,13 @@ from __future__ import unicode_literals
 
 from fabric.api import env, execute
 from fabric.network import disconnect_all
+from fabric.operations import _AttributeString
 
 from django.test import TestCase
 from django.contrib.auth.models import Permission
 
 from minke import sessions
+from minke.utils import UnicodeResult
 from minke.sessions import register
 from minke.sessions import Session
 from minke.sessions import UpdateEntriesSession
@@ -22,43 +24,38 @@ from .utils import create_localhost
 
 class TestSession(UpdateEntriesSession):
     def process(self):
-        update_cmd = 'echo "foobar"'
+        return getattr(self, 'test_' + self.session_data['test'])()
 
-        # message-tests
-        if self.session_data.has_key('message'):
-            # message-calls: valid, valid + stderr, invalid
-            self.execute('echo "hello wörld"')
-            self.execute('echo "hello wörld" &>2')
-            self.execute('[ 1 == 2 ]')
-
-        # update_field-tests
-        elif self.session_data.has_key('update'):
-            self.update_field('hostname', update_cmd)
-        elif self.session_data.has_key('update_regex'):
-            self.update_field('hostname', update_cmd, '(foo).+')
-        elif self.session_data.has_key('update_regex_fails'):
-            self.update_field('hostname', update_cmd, 'fails')
-
+    def test_execute(self):
+        # execute-calls: valid, valid + stderr, invalid
+        self.execute('echo "hello wörld"')
+        self.execute('echo "hello wörld" &>2')
+        self.execute('[ 1 == 2 ]')
         return self
+
+    def test_update(self):
+        self.update_field('hostname', 'echo "foobär"')
+        return self
+
+    def test_update_regex(self):
+        self.update_field('hostname', 'echo "foobär"', '(foo).+')
+        return self
+
+    def test_update_regex_fails(self):
+        self.update_field('hostname', 'echo "foobär"', 'fails')
+        return self
+
+    def test_unicode_result(self):
+        return self.run('(echo "hällo"; echo "wörld" 1>&2)')
+
+    def test_unicode_result_replace(self):
+        return self.run('(echo "hällo"; echo "wörld" 1>&2)', 'ascii')
 
 
 def process_session(session, hoststring):
     try: result = execute(session.process, hosts=[hoststring])
     finally: disconnect_all()
     return result[hoststring]
-
-
-class UpdateFieldTestSession(UpdateEntriesSession):
-    def process(self):
-        # update-field-calls: valid, valid + regex
-        cmd = 'echo "myhost"'
-        self.update_field(cmd, 'hostname')
-        return self
-
-
-# a monkey-class
-class Foobar(object):
-    pass
 
 
 class SessionTest(TestCase):
@@ -75,6 +72,10 @@ class SessionTest(TestCase):
         sessions.registry = self._registry[:]
 
     def test_01_register_session(self):
+        # a monkey-class
+        class Foobar(object):
+            pass
+
         # wrong session-class
         args = [Foobar]
         regex = 'must subclass Session'
@@ -111,7 +112,9 @@ class SessionTest(TestCase):
         self.reset_registry()
 
     def test_02_set_status(self):
+
         session = TestSession(self.host, self.server)
+
         session.set_status('error')
         self.assertTrue(session.status == 'error')
         session.set_status(session.ERROR)
@@ -125,6 +128,7 @@ class SessionTest(TestCase):
         self.assertRaises(ValueError, session.set_status, 'foobar')
 
     def test_03_cmd_format(self):
+
         # get formatted command-string (using players-attributes and session-data)
         cmd_format = '{hostname} {foo}'
         session_data = dict(foo='foo')
@@ -140,12 +144,10 @@ class SessionTest(TestCase):
 
     # TODO: skipIf-decorator if localhost cannot be connected
     def test_04_processing(self):
-        hoststring = self.host.hoststring
-        register(TestSession, Server)
 
         # test message-calls
-        session = TestSession(self.host, self.server, message=True)
-        session = process_session(session, hoststring)
+        session = TestSession(self.host, self.server, test='execute')
+        session = process_session(session, self.host.hoststring)
         news = session.news
         self.assertEqual(news[0].level, 'info')
         self.assertEqual(news[1].level, 'info')
@@ -155,21 +157,60 @@ class SessionTest(TestCase):
         self.assertRegex(news[2].text, 'code\[1\] +\[ 1 == 2 \]')
 
         # test update_field
-        session = TestSession(self.host, self.server, update=True)
-        session = process_session(session, hoststring)
-        self.assertEqual(session.player.hostname, 'foobar')
+        session = TestSession(self.host, self.server, test='update')
+        session = process_session(session, self.host.hoststring)
+        self.assertEqual(session.player.hostname, 'foobär')
 
         # test update_field with regex
-        session = TestSession(self.host, self.server, update_regex=True)
-        session = process_session(session, hoststring)
+        session = TestSession(self.host, self.server, test='update_regex')
+        session = process_session(session, self.host.hoststring)
         self.assertEqual(session.player.hostname, 'foo')
 
         # test update_field with failing regex
-        session = TestSession(self.host, self.server, update_regex_fails=True)
-        session = process_session(session, hoststring)
+        session = TestSession(self.host, self.server, test='update_regex_fails')
+        session = process_session(session, self.host.hoststring)
         self.assertEqual(session.news[0].level, 'error')
-        self.assertRegex(session.news[0].text, 'code\[0\] +echo "foobar"')
+        self.assertRegex(session.news[0].text, 'code\[0\] +echo "foobär"')
 
         # test update_field-call with invalid field
-        session = TestSession(self.host, self.server, update_invalid_field=True)
+        session = TestSession(self.host, self.server, test='update_invalid_field')
         self.assertRaises(AttributeError, session.update_field, 'nofield', 'echo')
+
+    def test_05_unicdoe_result(self):
+
+        # test with utf-8-encoding
+        session = TestSession(self.host, self.server, test='unicode_result')
+        result = process_session(session, self.host.hoststring)
+        self.assertEqual(type(result), UnicodeResult)
+        self.assertEqual(type(result.stdout), UnicodeResult)
+        self.assertEqual(type(result.stderr), unicode)
+        self.assertEqual(result, 'hällo')
+        self.assertEqual(result.stderr, 'wörld')
+
+        # test with ascii-encoding using replace
+        session = TestSession(self.host, self.server, test='unicode_result_replace')
+        result = process_session(session, self.host.hoststring)
+        self.assertEqual(result, 'h��llo')
+        self.assertEqual(result.stderr, 'w��rld')
+
+        # test UnicodeResult directly
+        attr_str = _AttributeString('hällo'.encode('utf-8'))
+        attr_str.command = 'any command ø'
+        attr_str.real_command = 'any real-command ø'
+        attr_str.stderr = 'wörld'.encode('utf-8')
+        attr_str.return_code = 0
+        attr_str.succeeded = True
+        attr_str.failed = False
+
+        result = UnicodeResult(attr_str, 'utf-8', 'replace')
+        self.assertEqual(type(result), UnicodeResult)
+        self.assertEqual(type(result.stdout), UnicodeResult)
+        self.assertEqual(type(result.stderr), unicode)
+        self.assertEqual(result, 'hällo')
+        self.assertEqual(result.stdout, 'hällo')
+        self.assertEqual(result.stderr, 'wörld')
+
+        # test with ascii-encoding using replace
+        result = UnicodeResult(attr_str, 'ascii', 'replace')
+        self.assertEqual(result, 'h��llo')
+        self.assertEqual(result.stderr, 'w��rld')
