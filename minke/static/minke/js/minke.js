d4bf02262555da2e1c7255f8b67be6ad90b9222c
Thomas Leichtfuß <leichtfuss@systopia.de>
2018-12-13 16:05:46 +0100

    refactored js-animation

--- minke/static/minke/js/minke.js
+++ minke/static/minke/js/minke.js
@@ -3,34 +3,30 @@
 var interval;
 var baseurl = window.location.protocol + '//' + window.location.host + '/minke' + window.location.pathname;
 
-function process_data(id, data) {
-    var proc_statuus = 'initialized running done';
-    var tr = $('#result_list input.action-select[value='+id+']').closest('tr')
-    tr.removeClass(proc_statuus).addClass(data.proc_status);
-    if (data.status) tr.addClass(data.status);
-    if (data.messages.length) {
-        var msg_tr = $('<tr></tr>').addClass('minke_news').addClass(data.status).hide();
-        var td = $('<td></td>').attr('colspan', 20);
-        var ul = $('<ul></ul>').addClass('messagelist');
-        tr.after(msg_tr.append(td.append(ul)));
-        $.each(data.messages, function(i, msg) {
-            ul.append($('<li></li>').addClass(msg.level).append(msg.html));
-        });
-        msg_tr.show('slow');
-    }
-}
-
 function process_result(result) {
     $.each(result, function(id, data){
-        process_data(id, data);
+        var proc_statuus = 'initialized running';
+        var tr = $('tr input.action-select[value='+id+']').closest('tr');
+        tr.removeClass(proc_statuus).addClass(data.proc_status);
+        if (data.status) tr.addClass(data.status);
+        if (data.messages.length) {
+            var msg_tr = $('<tr></tr>').addClass('minke_news').addClass(data.status).hide();
+            var td = $('<td></td>').attr('colspan', 20);
+            var ul = $('<ul></ul>').addClass('messagelist').hide();
+            tr.after(msg_tr.append(td.append(ul)));
+            $.each(data.messages, function(i, msg) {
+                ul.append($('<li></li>').addClass(msg.level).append(msg.html));
+            });
+            msg_tr.show(2000);
+            ul.slideDown('fast');
+        }
     });
 }
 
 function get_data(object_ids) {
     if (!object_ids) {
-        var object_ids = $('#result_list tr.initialized td.action-checkbox input, #result_list tr.running td.action-checkbox input')
-            .map(function() {return $(this).val()})
-            .get().join(',');
+        var object_ids = $('tr.initialized input.action-select, tr.running input.action-select')
+            .map(function() {return $(this).val()}).get().join(',');
     }
     if (object_ids) {
         var url = baseurl + '?object_ids=' + object_ids;
@@ -41,7 +37,7 @@ function get_data(object_ids) {
 }
 
 $(document).ready(function() {
-    var object_ids = $('#result_list tr:not(.done) td.action-checkbox input')
+    var object_ids = $('tr:not(.done) input.action-select')
         .map(function() {return $(this).val()})
         .get().join(',');
     get_data(object_ids);
