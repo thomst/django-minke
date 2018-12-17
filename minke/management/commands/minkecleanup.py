1d66bbb61c40766a505d0174f9598d4e3a78f150
Thomas Leichtfuß <leichtfuss@systopia.de>
2018-12-17 01:25:27 +0100

    added clear-sessions-option form minkecleanup-command

--- minke/management/commands/minkecleanup.py
+++ minke/management/commands/minkecleanup.py
@@ -18,11 +18,20 @@ class Command(BaseCommand):
             '--clear-current-sessions',
             action='store_true',
             help='Set current to False for all sessions.')
+        parser.add_argument(
+            '--clear-sessions',
+            action='store_true',
+            help='Delete all sessions.')
 
     def handle(self, *args, **options):
         if options['release_locks']:
             Host.objects.release_lock()
+
         elif options['clear_current_sessions']:
             BaseSession.objects.update(current=False)
+
+        elif options['clear_sessions']:
+            BaseSession.objects.delete()
+
         else:
             self.print_help('manage.py', 'minkecleanup')
