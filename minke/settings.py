e60f7f59045a777510869422b6c294a31d780027
Thomas Leichtfuß <leichtfuss@systopia.de>
2019-03-23 19:10:54 +0100

    added some default-settings for invoke

--- minke/settings.py
+++ minke/settings.py
@@ -10,6 +10,10 @@ MINKE_FABRIC_FORM = getattr(settings, 'MINKE_FABRIC_FORM', None)
 MINKE_CLI_USER = getattr(settings, 'MINKE_CLI_USER', 'admin')
 MINKE_FABRIC_CONFIG = getattr(settings, 'MINKE_FABRIC_CONFIG', FabricConfig())
 
+# set defaults for fabric-/invoke-config
+MINKE_FABRIC_CONFIG.run.hide = True
+MINKE_FABRIC_CONFIG.run.warn = True
+
 # All config-vars starting with FABRIC_ will be loaded into our fabric-config...
 fabric_data = dict([(k[7:].lower(), getattr(settings, k)) for k in dir(settings) if k.startswith('FABRIC')])
 MINKE_FABRIC_CONFIG.load_snakeconfig(fabric_data)
