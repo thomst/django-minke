84b2f7ac02ea7381ab47f2737cb6f8cf2e269e5f
Thomas Leichtfuß <leichtfuss@systopia.de>
2019-06-10 17:47:07 +0200

    use an inclusion-tag for the session-info-row

--- minke/templatetags/minke.py
+++ minke/templatetags/minke.py
@@ -11,6 +11,10 @@ register = Library()
 
 @register.inclusion_tag('minke/change_list_results.html', takes_context=True)
 def minke_result_list(context):
+    """
+    Replacement of django's result_list-inclusion_tag.
+    We use our own template and pass sessions as context.
+    """
     cl = context['cl']
     cxt = result_list(cl)
     sessions = MinkeSession.objects.get_currents(context['user'], cl.result_list)
@@ -19,15 +23,31 @@ def minke_result_list(context):
     cxt['results'] = zip(cxt['results'], sorted)
     return cxt
 
+
 @register.inclusion_tag('minke/admin_actions.html', takes_context=True)
 def minke_admin_actions(context):
+    """
+    Same as django's admin_action-inclusion_tag but with our own template.
+    """
     return admin_actions(context)
+
 
 @register.inclusion_tag('minke/session_bar.html', takes_context=True)
 def minke_session_bar(context):
+    """
+    Render the session-select-form from within the actions-template.
+    """
     cl = context['cl']
     request = context['request']
     form = cl.model_admin.get_session_select_form(request)
     context['session_select_form'] = form
     context['current_sessions'] = cl.model_admin.get_current_sessions(request)
+    return context
+
+
+@register.inclusion_tag('minke/session.html', takes_context=True)
+def minke_session(context):
+    """
+    Render the session-info-row.
+    """
     return context
